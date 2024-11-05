6ba1b8f2c5dd617ba1f532d5f525efe2fbed844e
Gustavo Rizerio Souza <18-04479@grupoccr.com.br>
2024-11-05 10:30:16 -0300

    feat: adiciona classe de configuração AppConfig

---
+++ src/main/java/com/ccr/first_spring_app/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.ccr.first_spring_app.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
